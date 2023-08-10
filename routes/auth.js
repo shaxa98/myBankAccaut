67826ee2ca81bd63d858458d871f708482a61b98
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-08-10 11:00:15 +0500

    add solution

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+// login, emailki password mibiyod tekshiri kadan darkor
+router.post("/login", () => {});
+//register
+router.post("/register", () => {});
+//change password
+router.patch("/password", () => {});
 module.exports = router;
