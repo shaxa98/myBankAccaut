41855ec85fe3cbd6b56e3abb1b1ff13d83053c5a
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-08-13 17:47:02 +0500

    hh

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,74 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user-module");
+
 // login, emailki password mibiyod tekshiri kadan darkor
-router.post("/login", async (res, req, next) => {
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
   try {
-    const users = await User.create(req.body);
-    console.log(users);
-    res.status(200).json(login);
+    const user = await User.findOne({ email });
+
+    if (user) {
+      if (user.password === password) {
+        res.json({ message: "Login successful!" });
+      } else {
+        res
+          .status(401)
+          .json({ error: "Incorrect password. Please try again." });
+      }
+    } else {
+      res
+        .status(404)
+        .json({ error: "User not found. Please check your email." });
+    }
   } catch (error) {
-    next(error);
+    res.status(500).json({ error: "Internal server error." });
   }
 });
 //register
-router.post("/register", () => {});
+
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(409).json({ error: "User with this email already exists." });
+    } else {
+      const newUser = new User({ email, password });
+      await newUser.save();
+      res.json({ message: "Registration successful!" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
 //change password
-router.patch("/password", () => {});
+
+router.patch("/password", async (req, res) => {
+  const { email, currentPassword, newPassword } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res
+        .status(404)
+        .json({ error: "User not found. Please check your email." });
+    } else {
+      if (user.password !== currentPassword) {
+        res.status(401).json({ error: "Incorrect current password." });
+      } else {
+        user.password = newPassword;
+        await user.save();
+        res.json({ message: "Password changed successfully!" });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
 module.exports = router;
