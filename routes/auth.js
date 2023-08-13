f23bad6f9e836b4f8cd42f2a18cdcaf544cbaf1f
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-08-13 17:46:44 +0500

    add new commands

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user-module");
 // login, emailki password mibiyod tekshiri kadan darkor
-router.post("/login", () => {});
+router.post("/login", async (res, req, next) => {
+  try {
+    const users = await User.create(req.body);
+    console.log(users);
+    res.status(200).json(login);
+  } catch (error) {
+    next(error);
+  }
+});
 //register
 router.post("/register", () => {});
 //change password
