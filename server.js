153a95d49d6717225437dea6a27358744b14c501
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-08-19 22:41:39 +0500

    add port env

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.use((err, req, res, next) => {
   res.send(err.message);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
