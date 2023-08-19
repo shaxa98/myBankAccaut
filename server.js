83c00d131c296bca9c00b717c84b75d0cd7e656b
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-08-19 22:44:46 +0500

    add port varevbl

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
   res.status(500);
   res.send(err.message);
 });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
